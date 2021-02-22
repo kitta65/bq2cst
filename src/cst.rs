ddee7e07d5c61cf88108f826fe0fc905f2d2b2fa
dr666m1 <skndr666m1@gmail.com>
2021-02-22 23:27:49 +0900

    cst to_string done

--- src/cst.rs
+++ src/cst.rs
@@ -15,13 +15,57 @@ pub struct Node {
 }
 
 impl Node {
-    fn to_str(&self, indent: usize) -> &str {
-        self.token.literal.as_str()
+    fn to_string(&self, indent: usize, is_array: bool) -> String {
+        let mut res = Vec::new();
+        // push `self: xxx` or `- self: xxx`
+        if is_array {
+            res.push(format!(
+                "-{}self: {}",
+                " ".repeat(indent * 2 - 1),
+                self.token.literal.clone()
+            ))
+        } else {
+            res.push(format!(
+                "{}self: {}",
+                " ".repeat(indent * 2),
+                self.token.literal.clone()
+            ))
+        }
+        // prepare keys
+        let mut keys = Vec::new();
+        for key in self.children.keys() {
+            keys.push(key);
+        }
+        keys.sort();
+        for key in keys {
+            // push `key:`
+            res.push(format!("{}:", key.clone()));
+            let child_string = match self.children.get(key).unwrap() {
+                Children::Node(n) => n.to_string(indent + 1, false),
+                Children::NodeVec(ns) => {
+                    let mut children = Vec::new();
+                    for n in ns {
+                        children.push(n.to_string(indent + 1, true));
+                    }
+                    children.join("\n")
+                }
+            };
+            res.push(child_string);
+        }
+        res.join("\n")
+    }
+    fn push_node(&mut self, key: &str, node: Node) {
+        self.children.insert(key.to_string(), Children::Node(node));
+    }
+    fn push_node_vec(&mut self, key: &str, nodes: Vec<Node>) {
+        self.children
+            .insert(key.to_string(), Children::NodeVec(nodes));
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     fn new_node(literal: &str) -> Node {
         // this function is only used in test_to_string.
         // so line and column are 0.
@@ -30,9 +74,20 @@ mod tests {
             children: HashMap::new(),
         }
     }
-    use super::*;
+    #[test]
     fn test_to_string() {
-        let node = new_node("root");
-        assert_eq!(node.to_str(0), "root")
+        let mut root = new_node("root");
+        root.push_node("key1", new_node("child1"));
+        root.push_node_vec("key2", vec![new_node("child2"), new_node("child3")]);
+        assert_eq!(
+            root.to_string(0, false),
+            "\
+self: root
+key1:
+  self: child1
+key2:
+- self: child2
+- self: child3"
+        )
     }
 }
