6c15541d0f4fdbeb977c835116c4c79d16f18c02
dr666m1 <skndr666m1@gmail.com>
2021-09-11 16:57:23 +0900

    Don't set_panic_hook by default.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 cd $(dirname $0)
 cargo test && \
 wasm-pack test --node && \
-wasm-pack build --target nodejs --scope dr666m1 && \
+wasm-pack build --target nodejs --scope dr666m1 -- --no-default-features && \
 wasm-pack publish --access public
