197b82b744ef028e6b0505c1d950b054ff62da6b
dr666m1 <skndr666m1@gmail.com>
2022-05-14 18:37:23 +0900

    Remove deploy.sh
    Now that ci is ready, it is not needed.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-cargo test && \
-wasm-pack test --node && \
-wasm-pack build --target nodejs --scope dr666m1 && \
-cp LICENSE* pkg/ && \
-wasm-pack publish --access public
