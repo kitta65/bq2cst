f35a43a952c2fb7544c326bfb4639e8fe52e3010
dr666m1 <skndr666m1@gmail.com>
2022-10-15 19:59:43 +0900

    use serde-wasm-bindgen
    this is recommended approach.
    https://rustwasm.github.io/wasm-bindgen/reference/arbitrary-data-with-serde.html#serializing-and-deserializing-arbitrary-data-into-and-from-jsvalue-with-serde

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+// you can ignore diagnostic message
+// https://github.com/rustwasm/wasm-bindgen/issues/2882
 #[wasm_bindgen(skip_typescript)]
 pub fn parse(code: String) -> Result<JsValue, JsValue> {
     utils::set_panic_hook();
@@ -16,18 +18,18 @@ pub fn parse(code: String) -> Result<JsValue, JsValue> {
     let mut p = parser::Parser::new(match l.tokenize_code() {
         Ok(tokens) => tokens,
         Err(bq2cst_error) => {
-            return Err(JsValue::from_serde(&bq2cst_error)
+            return Err(serde_wasm_bindgen::to_value(&bq2cst_error)
                 .expect("Problem converting error struct to json."))
         }
     });
     let stmts = match p.parse_code() {
         Ok(stmts) => stmts,
         Err(bq2cst_error) => {
-            return Err(JsValue::from_serde(&bq2cst_error)
+            return Err(serde_wasm_bindgen::to_value(&bq2cst_error)
                 .expect("Problem converting error struct to json."))
         }
     };
-    Ok(JsValue::from_serde(&stmts).expect("Problem converting stmts to json."))
+    Ok(serde_wasm_bindgen::to_value(&stmts).expect("Problem converting stmts to json."))
 }
 
 #[wasm_bindgen(skip_typescript)]
@@ -37,9 +39,9 @@ pub fn tokenize(code: String) -> Result<JsValue, JsValue> {
     let tokens = match l.tokenize_code() {
         Ok(tokens) => tokens,
         Err(bq2cst_error) => {
-            return Err(JsValue::from_serde(&bq2cst_error)
+            return Err(serde_wasm_bindgen::to_value(&bq2cst_error)
                 .expect("Problem converting error struct to json."))
         }
     };
-    Ok(JsValue::from_serde(&tokens).expect("Problem converting tokens to json."))
+    Ok(serde_wasm_bindgen::to_value(&tokens).expect("Problem converting tokens to json."))
 }
